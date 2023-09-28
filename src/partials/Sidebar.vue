f84e5804486ec72a586139f6d39ad2dc93bd8b67
Andre Tri Ramadana <32930152+andre12001@users.noreply.github.com>
2023-09-28 13:57:25 +0700

    Update Sidebar.vue

--- src/partials/Sidebar.vue
+++ src/partials/Sidebar.vue
@@ -94,12 +94,4 @@
       </aside>  
 </template>
 
- <script>
-     'use strict';
 
-$(document).ready(function(){
-    $("#data-sidebar-btn").click(function(){
-        $("#sidebar").toggleClass("active");
-    });
-});
-</script>
